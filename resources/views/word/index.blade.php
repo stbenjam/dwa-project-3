67471e64ae5038aa984950be8ec4b68b91fe3b69
Stephen Benjamin <stephen@bitbin.de>
2017-11-01 15:16:52 -0400

    Oops, forgot required form css

--- resources/views/word/index.blade.php
+++ resources/views/word/index.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.master')
 
+<link rel="stylesheet" href="css/required.css">
+
+@push('head')
+    <link rel="stylesheet" href="css/required.css">
+@endpush
+
 @section('content')
 
 @if(count($errors) > 0)
